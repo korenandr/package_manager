9cd6c633376e2eb5be28a437a50efada1d851c57
akorenevich <akorenevich@octonion.by>
2021-06-20 17:12:05 +0300

    Added tests for package functions

--- test/PackageTest.cpp
+++ test/PackageTest.cpp
@@ -9,7 +9,7 @@ TEST(PackageTest, TEST_CREATE)
     Package package;
 
     ASSERT_EQ(package.create("home"), OR::NO_ERROR);
-    ASSERT_EQ(package.create("home"), OR::NO_ERROR);
+    ASSERT_NE(package.create("home"), OR::NO_ERROR);
 
     ASSERT_EQ(package.create("media"), OR::NO_ERROR);
 }
@@ -23,10 +23,22 @@ TEST(PackageTest, TEST_ADD)
     ASSERT_EQ(package.add("home/project", "teslasuit"), OR::NO_ERROR);
     ASSERT_EQ(package.add("home/project/teslasuit", "build"), OR::NO_ERROR);
 
+
     ASSERT_EQ(package.create("media"), OR::NO_ERROR);
     ASSERT_EQ(package.add("media", "device"), OR::NO_ERROR);
     ASSERT_EQ(package.add("media/device", "input"), OR::NO_ERROR);
     ASSERT_EQ(package.add("media/device/input", "headphones"), OR::NO_ERROR);
+
+
+    ASSERT_EQ(package.create("device"), OR::NO_ERROR);
+    ASSERT_EQ(package.add("device", "headphones"), OR::NO_ERROR);
+    ASSERT_NE(package.add("device", "headphones"), OR::NO_ERROR);
+
+    ASSERT_EQ(package.add("device", "microphone"), OR::NO_ERROR);
+    ASSERT_NE(package.add("device", "microphone"), OR::NO_ERROR);
+    
+
+    ASSERT_NE(package.add("device/not/created/folder", "microphone"), OR::NO_ERROR);
 }
 
 TEST(PackageTest, TEST_REMOVE)
@@ -38,5 +50,24 @@ TEST(PackageTest, TEST_REMOVE)
     ASSERT_EQ(package.add("home/project", "teslasuit"), OR::NO_ERROR);
     ASSERT_EQ(package.add("home/project/teslasuit", "build"), OR::NO_ERROR);
 
-    ASSERT_EQ(package.remove("home/project/teslasuit"), OR::NO_ERROR);
+    ASSERT_EQ(package.remove("home/project/teslasuit/build"), OR::NO_ERROR);
+    ASSERT_NE(package.remove("home/project/teslasuit/build"), OR::NO_ERROR);
+
+    ASSERT_NE(package.remove("home/not/created/folder", "microphone"), OR::NO_ERROR);
+}
+
+TEST(PackageTest, TEST_PRINT)
+{
+    Package package;
+
+    ASSERT_EQ(package.create("home"), OR::NO_ERROR);
+    ASSERT_EQ(package.add("home", "project"), OR::NO_ERROR);
+    ASSERT_EQ(package.add("home/project", "teslasuit"), OR::NO_ERROR);
+    ASSERT_EQ(package.add("home/project/teslasuit", "build"), OR::NO_ERROR);
+
+    ASSERT_EQ(package.print("home/project"), OR::NO_ERROR);
+    ASSERT_EQ(package.print("home/project/teslasuit"), OR::NO_ERROR);
+
+    ASSERT_NE(package.print("home/not/created/folder"), OR::NO_ERROR);
+    ASSERT_NE(package.print("home/another/not/created/folder"), OR::NO_ERROR);
 }
